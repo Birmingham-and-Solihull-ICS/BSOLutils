86c0d61bb6d2f798773341bea98a9eb3875b13cb
Chris Mainey <c.mainey1@nhs.net>
2026-02-27 22:59:22 +0000

    update ISR test to get round issue loading with data argument.

--- tests/testthat/test-ISR_deprivation.R
+++ tests/testthat/test-ISR_deprivation.R
@@ -1,8 +1,23 @@
 ## Unit tests for `ISR_deprivation()`
 
+example_dt <-
+  data.frame(
+    age_related_seed = rep(seq(0.8, 1.2, 0.07), 32),
+    age_group_code = rep(seq(1,6), 32),
+    sex_group_code = rep(rep(c(1,2), each = 6), 16),
+    imd_code = rep(c(1,2,3,4,5,999), 32)
+  )
+
+
+set.seed(123)
+
+example_dt$numerator <- rpois(192,example_dt$age_related_seed * 30)
+example_dt$denominator <- rpois(192,example_dt$age_related_seed * 85)
+
+ISR_example <- example_dt[, names(example_dt) != "age_related_seed", drop = FALSE]
 
 test_that("ISR_deprivation returns expected structure and positive ratios", {
-  data(ISR_example)
+  #data(ISR_example)
   out <- ISR_deprivation(ISR_example)
   expect_s3_class(out, "data.frame")
   expect_equal(names(out), c("imd_quintile","ratio","lowerCI","upperCI"))
@@ -12,7 +27,7 @@ test_that("ISR_deprivation returns expected structure and positive ratios", {
 
 
 test_that("ISR_deprivation can toggle age/sex predictors", {
-  data(ISR_example)
+  #data(ISR_example)
   out1 <- ISR_deprivation(ISR_example, age = FALSE, sex = TRUE)
   out2 <- ISR_deprivation(ISR_example, age = TRUE, sex = TRUE)
   expect_true(nrow(out1) == 5)
