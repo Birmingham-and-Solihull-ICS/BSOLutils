42cd98297ce1df6f51d45916b71169b18e0f9da5
Chris Mainey <c.mainey1@nhs.net>
2026-02-28 07:57:51 +0000

    fix for failing test

--- tests/testthat/test-ISR_deprivation_plot.R
+++ tests/testthat/test-ISR_deprivation_plot.R
@@ -1,5 +1,20 @@
 ## Unit tests for `ISR_deprivation_plot()`
 
+example_dt <-
+  data.frame(
+    age_related_seed = rep(seq(0.8, 1.2, 0.07), 32),
+    age_group_code = rep(seq(1,6), 32),
+    sex_group_code = rep(rep(c(1,2), each = 6), 16),
+    imd_code = rep(c(1,2,3,4,5,999), 32)
+  )
+
+
+set.seed(123)
+
+example_dt$numerator <- rpois(192,example_dt$age_related_seed * 30)
+example_dt$denominator <- rpois(192,example_dt$age_related_seed * 85)
+
+ISR_example <- example_dt[, names(example_dt) != "age_related_seed", drop = FALSE]
 
 test_that("ISR_deprivation_plot returns ggplot object with expected layers", {
   data(ISR_example)
